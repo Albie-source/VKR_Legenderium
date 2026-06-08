540339363ba1bf78b2372d7811a572c1f9003bb4
Albie-source <azagrevskaa@gmail.com>
2026-06-08 23:01:06 +0900

    Новая версия с более сильным сценарием использования

---
+++ prisma/migrations/20260607184632_newversion/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX "genres_name_trgm";
+
+-- DropIndex
+DROP INDEX "materials_full_text_trgm";
+
+-- DropIndex
+DROP INDEX "materials_short_desc_trgm";
+
+-- DropIndex
+DROP INDEX "materials_title_trgm";
+
+-- DropIndex
+DROP INDEX "peoples_name_trgm";
+
+-- DropIndex
+DROP INDEX "regions_name_trgm";
